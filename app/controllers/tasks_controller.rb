d729d3ac5a9617c608bd327d20270fe1d379ab70
Jennifer Owens <jmo.jennifer@gmail.com>
2016-09-28 15:18:20 -0700

    Baseline and Wave 1 complete--forgot to commit throughout initial phases

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,55 @@
+class TasksController < ApplicationController
+
+  def index
+    @name = "Jennifer"
+
+    @all_tasks = [
+      {name: "Petcare",
+        description: "Give Jassy her dinner and lovins",
+        completion_status: "Pending",
+        completion_date: nil,
+        id: 1
+      },
+      {
+        name: "Homework",
+        description: "Do readings and play with zombies",
+        completion_status: "Pending",
+        completion_date: nil,
+        id: 2
+      },
+      {
+        name: "Rest",
+        description: "Get some ZZZs",
+        completion_status: "Complete",
+        completion_date: "9-26-2016",
+        id: 3
+        }]
+
+  end
+
+  def show
+    all_tasks = [
+      {name: "Petcare",
+        description: "Give Jassy her dinner and lovins",
+        completion_status: "Pending",
+        completion_date: nil,
+        id: 1
+      },
+      {
+        name: "Homework",
+        description: "Do readings and play with zombies",
+        completion_status: "Pending",
+        completion_date: nil,
+        id: 2
+      },
+      {
+        name: "Rest",
+        description: "Get some ZZZs",
+        completion_status: "Complete",
+        completion_date: "9-26-2016",
+        id: 3
+        }]
+
+        @task = all_tasks[params[:id].to_i - 1]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
+  get 'tasks' => 'tasks#index'
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
