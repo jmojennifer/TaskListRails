0d0a8bd8ffa8bad57577d201c6b5d1efee5859ff
Jennifer Owens <jmo.jennifer@gmail.com>
2016-09-29 15:44:01 -0700

    Migrated schema

--- db/migrate/20160929223301_create_tasks.rb
+++ db/migrate/20160929223301_create_tasks.rb
@@ -1,10 +1,10 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.name null: false
-      t.description
-      t.completion_status null:false
-      t.completion_date
+      t.string :name, null: false
+      t.string :description
+      t.string :completion_status, null: false
+      t.date :completion_date
       t.timestamps null: false
     end
   end
