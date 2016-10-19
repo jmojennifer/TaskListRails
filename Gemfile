cda97b8554bdc7a4daf2429549dae6c0b2c4e1e9
Jennifer Owens <jmo.jennifer@gmail.com>
2016-10-18 20:00:24 -0700

    Added gems/code for SimpleCov

--- Gemfile
+++ Gemfile
@@ -40,6 +40,11 @@ group :development, :test do
   gem 'dotenv-rails'
 end
 
+group :test do
+  gem 'simplecov'
+  gem 'minitest-reporters'
+end
+
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+require 'simplecov'
+SimpleCov.start
+Rails.application.eager_load!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
+  Minitest::Reporters.use!
 end
